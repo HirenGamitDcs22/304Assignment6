897c2f4eaabfbfc4bb05051d26a862fe84b12cb8
Gamit Hiren <91384225+HirenGamitDcs22@users.noreply.github.com>
2021-12-05 23:02:07 +0530

    Create product.js

---
+++ models/product.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const productSchema=mongoose.Schema({
+    id:String,
+    name:String,
+    gen_features:[{
+        sim_size:String,
+        sim_type:[String],
+        net_type:[String],
+        battery_type:String,
+        sensors:[String],
+        rem_battery:String
+    }],
+    platform_performance:[{
+        os:String,
+        processor:String,
+        RAM:String,
+        graphics:String
+    }],
+    camera:[{ 
+        primary_camera:String,
+        front_camera:String,
+        flash:String,
+        zoom:String,
+        video_rec:[String]
+    }],
+    display:[{ 
+        resolution:String,
+        feature:String
+    }],
+    storage:[{
+        int_memory:String,
+        exp_memory:String
+    }],
+    connectivity_features:[{ 
+        usb:String,
+        wifi:String,
+        bluetooth:String
+    }],
+    price:String,
+    variant:[String],
+    brand_id:String
+});
+
+const productModel = mongoose.model("product",productSchema,"product");
+
+module.exports = productModel;
