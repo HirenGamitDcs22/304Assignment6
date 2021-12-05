61d97490b540cd28a013bcbe17c4db7c3ed3a3fb
Gamit Hiren <91384225+HirenGamitDcs22@users.noreply.github.com>
2021-12-06 01:14:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,5 +15,5 @@ const productRoutes=require("./routes/product");
 app.use("/brand",brandRoutes);
 app.use("/product",productRoutes);
 
-app.get('/', (req, res) => res.send('Welcome to Mobile App !'))
+app.get('/', (req, res) => res.send('Welcome to Mobile Detali Api !'))
 app.listen(port, () => console.log(`Server started on port ${port}!`))
